4ba10580c5bd8c8b49f7a7de5b11fe733afd082e
Squ1ki-dev <70817453+Squ1ki-dev@users.noreply.github.com>
2020-10-28 14:12:08 +0200

    Add files via upload

---
+++ SafingLoading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SafingLoading : MonoBehaviour 
+{
+	
+	public Slider slider;
+	public int Level;
+
+	private int slidV;
+	private float timer;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		slider.value = slidV;
+		timer += 2  * Time.deltaTime;
+		if (timer >= 1) {
+			slidV += 7;
+			timer = 0;
+		}
+		if (slider.value >= 100) {
+			SceneManager.LoadScene (Level);
+		}
+	}
+}
